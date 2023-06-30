aaa63e2aebe79f25e63910630a9673e5bc3bdf59
Andrey Berdnik <berdnik26@gmail.com>
2023-06-30 12:01:54 +0300

    Echo bot

--- first_file.py
+++ first_file.py
@@ -1,28 +1,42 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from aiogram.client.session.aiohttp import AiohttpSession
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+API_TOKEN: str = '6294776237:AAEQLWQUSq9dS9Zq-DS4nYiphKR12xHb7Ds'
+
+session = AiohttpSession(proxy="http://10.26.145.5:8080/")
+#bot = Bot('42:token', session=session)
 
 
-API_URL: str = 'https://api.telegram.org/bot'
-BOT_TOKEN: str = ''
-TEXT: str = 'Ура! Классный апдейт!'
-MAX_COUNTER: int = 100
-
-offset: int = -2
-counter: int = 0
-chat_id: int
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN, session=session)
+dp: Dispatcher = Dispatcher()
 
 
-while counter < MAX_COUNTER:
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    # Если есть необходимость отправить в другой чат (пользователю)
+    # await bot.send_message(message.chat.id, message.text)
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
 
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
 
-    time.sleep(2)
-    counter += 1
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
