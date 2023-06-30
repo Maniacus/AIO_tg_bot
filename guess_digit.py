cc4351f6c703a2677bad878592957ad7ed3ce3eb
Andrey Berdnik <berdnik26@gmail.com>
2023-06-30 15:17:25 +0300

    Удален токен

--- guess_digit.py
+++ guess_digit.py
@@ -8,7 +8,7 @@ from aiogram.client.session.aiohttp import AiohttpSession
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
-BOT_TOKEN: str = '6294776237:AAEQLWQUSq9dS9Zq-DS4nYiphKR12xHb7Ds'
+BOT_TOKEN: str = ''
 session = AiohttpSession(proxy="http://10.26.145.5:8080/")
 
 
